891a11b000316729a1c63025f42facb3164e55f4
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-04-11 16:56:49 -0700

    Finishing the data models portion

--- imager_images/models.py
+++ imager_images/models.py
@@ -1,10 +1,46 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 def user_directory_path(instance, filename):
     return "user_{0}/{1}".format(instance.user.id, filename)
 
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
+PUBLISH_OPTIONS = [("private", "Private"),
+                   ("shared", "Shared"),
+                   ("public", "Public")]
+
+
 class Photo(models.Model):
-    # file = models.ImageField(upload_to=user_directory_path, max_length=100)
     file = models.ImageField(upload_to='user_photos')
+    owner = models.OneToOneField(User, related_name="photo", null=False, default=None)
+    title = models.CharField(max_length=255, default=None)
+    description = models.TextField(blank=True)
+    date_uploaded = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    published = models.CharField(
+        max_length=7, choices=PUBLISH_OPTIONS, default="public")
+
+
+class Album(models.Model):
+    """A model that houses and points to many photos"""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    photo_set = models.ManyToManyField(Photo, related_name="photos_of")
+    title = models.CharField(max_length=255, default=None)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    published = models.CharField(max_length=7, choices=PUBLISH_OPTIONS, default="public")
+
 
--- imageshare/urls.py
+++ imageshare/urls.py
@@ -18,9 +18,10 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from . import views
 
 urlpatterns = [
-    url(r'^/$', views.home_view, name="homepage"),
+    url(r'^$', views.home_view, name="homepage"),
     url(r'^admin/', admin.site.urls),
     url(r'^profile/', include('imager_profile.urls'))
 ]
